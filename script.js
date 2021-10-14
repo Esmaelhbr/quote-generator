f40af21e89c1b59277e8105d66465ae5744408d1
Esmaelhbr <esmael7@live.com>
2021-10-13 23:23:39 -0400

    fully working app

--- script.js
+++ script.js
@@ -1,10 +1,34 @@
+
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
+
 let apiQuotes = [];
 //sho new quote
 function newQuote(){
 	//pick a random quote from apiquotes array 
 	const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
-	console.log(quote);
+	
+	if(!quote.author){
+		authorText.textContent= 'Unknown';
+	}else{
+		authorText.textContent = quote.author;
+	}
+	
+
+	// check quote lengthto determine styling
+	if(quote.text.length > 120){
+		quoteText.classList.add('long-quote')
+	}else{
+		quote.classList.remove('long-quote')
+	}
+	quoteText.textContent = quote.text;
 }
+
+	
+	
 
 
 // get quotes from api
@@ -22,6 +46,18 @@ async function getQuotes(){
 		//handle error
 	}
 }
+
+
+//Tweet quote
+
+function tweetQuote(){
+	const twitterUrl =`https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+	window.open(twitterUrl, '_blank');
+}
+
+//event listeners
+newQuoteBtn.addEventListener('click', newQuote);
+twitterBtn.addEventListener('click', tweetQuote)
 
 
 //on load
