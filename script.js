babeaa752ff005d3c08e0eb48170f4a4b3edbc28
Esmaelhbr <esmael7@live.com>
2021-10-14 13:10:17 -0400

    fixed all the issues with the api

--- script.js
+++ script.js
@@ -1,64 +1,70 @@
-
 const quoteContainer = document.getElementById('quote-container');
 const quoteText = document.getElementById('quote');
 const authorText = document.getElementById('author');
 const twitterBtn = document.getElementById('twitter');
 const newQuoteBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
 
-let apiQuotes = [];
-//sho new quote
-function newQuote(){
-	//pick a random quote from apiquotes array 
-	const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
-	
-	if(!quote.author){
-		authorText.textContent= 'Unknown';
-	}else{
-		authorText.textContent = quote.author;
-	}
-	
 
-	// check quote lengthto determine styling
-	if(quote.text.length > 120){
-		quoteText.classList.add('long-quote')
-	}else{
-		quote.classList.remove('long-quote')
-	}
-	quoteText.textContent = quote.text;
+// Show Loading
+function loading() {
+    loader.hidden = false;
+    quoteContainer.hidden = true;
 }
 
+// Hide Loading
+function complete() {
+    if (!loader.hidden) {
+        quoteContainer.hidden = false;
+        loader.hidden = true;
+    }
+}
+//get quote  from api
+
+async function getQuote(){
+	loading();
 	
-	
+	const proxyUrl = 'https://whispering-tor-04671.herokuapp.com/'
+	const apiUrl ='http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
 
-
-// get quotes from api
-
-async function getQuotes(){
-	const apiUrl ='https://type.fit/api/quotes';
 	try{
-		const response = await fetch(apiUrl);
-		apiQuotes = await response.json();
+		const response = await fetch(proxyUrl + apiUrl);
+		const data = await response.json();
 
-		newQuote();
+		if(data.quoteAuthor ===''){
+			authorText.innerText ='Unknow';
+		}else{
+			authorText.innerText = data.quoteAuthor;
+		}
+		//reduce font size for long size
+		if(data.quoteText.length > 120){
+			quoteText.classList.add('long-quote');
+		}else{
+			quoteText.classList.remove('long-quote');
+		}
+		quoteText.innerText = data.quoteText;
 
-
+		//stop loader show the quote
+		complete();
 	}catch(error){
-		//handle error
+		getQuote();
+		
 	}
-}
 
+}
 
 //Tweet quote
-
 function tweetQuote(){
-	const twitterUrl =`https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+	const quote=quoteText.innerText;
+	const author= authorText.innerText;
+	const twitterUrl =`https://twitter.com/intent/tweet?text=${quote} - ${author}`;
 	window.open(twitterUrl, '_blank');
 }
 
 //event listeners
-newQuoteBtn.addEventListener('click', newQuote);
+//event listeners
+newQuoteBtn.addEventListener('click', getQuote);
 twitterBtn.addEventListener('click', tweetQuote)
 
-
 //on load
-getQuotes();
+ getQuote();
